5b7dc33effdb16760172546683712d8eb1c6aeae
Yug Sondagar <yug.sondagar11@gmail.com>
2025-12-27 16:24:03 +0530

    Switch to environment variables for security

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_FROM_NUMBER = os.getenv('TWILIO_FROM_NUMBER')
+EMERGENCY_NUMBER = os.getenv('EMERGENCY_NUMBER')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
